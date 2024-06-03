fa0d0b0e9f85b1a4ffe9062332b354db4d66cd22
rexsum420 <132636428+rexsum420@users.noreply.github.com>
2024-06-03 04:36:52 -0500

    Update middleware.py

--- social_network/middleware.py
+++ social_network/middleware.py
@@ -4,12 +4,11 @@ from logs.models import UserLog
 
 class UserActivityLoggingMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        if request.user.is_authenticated:
-            if request.path == '/api-token-auth/' and request.method == 'POST':
-                UserLog.objects.create(user=request.user, action='Logged in', timestamp=datetime.now())
-                self.update_ip_address(request)
-            elif request.path == '/logout/':
-                UserLog.objects.create(user=request.user, action='Logged out', timestamp=datetime.now())
+        if request.path == '/api-token-auth/' and request.method == 'POST':
+            UserLog.objects.create(user=request.user, action='Logged in', timestamp=datetime.now())
+            self.update_ip_address(request)
+        elif request.path == '/logout/':
+            UserLog.objects.create(user=request.user, action='Logged out', timestamp=datetime.now())
 
     def update_ip_address(self, request):
         ip_address = request.META.get('REMOTE_ADDR')
